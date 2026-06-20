51319ce7b9af59a21708730b06273e31fb5df54e
francoism90 <francoism90@users.noreply.github.com>
2026-06-20 23:28:53 +0200

    Introduce org.freedesktop.Sdk.Extension.podma

--- src/io.github.francoism90.HelloFlatpak/hello-flatpak.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-echo "Hello from io.github.francoism90.HelloFlatpak"
-echo "This test app exists to publish appstream metadata in your Flatpak repo."
-printf "\nFlatpak ID: %s\n" "${FLATPAK_ID:-unknown}"
-printf "Sandbox: %s\n" "${FLATPAK_SANDBOX_DIR:-unknown}"
-printf "\nPress Enter to exit..."
-read -r _
